04ec9de4c5c1a241849e68084a7f8aab636b56ca
MashaKujou <mjvillacantos@gmail.com>
2024-10-09 14:38:50 +0800

    Update FileManager.java

--- DIT_FinalProject/FileManager.java
+++ DIT_FinalProject/FileManager.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 public class FileManager {
     private static final String FILE_PATH = "src/accounts.txt";
 
